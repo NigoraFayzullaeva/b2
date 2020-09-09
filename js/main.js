216a5e830de5d190267f9df1cee068d2c027c812
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2020-09-09 20:15:58 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.hamburger, .menu').click(function () {
+    if ($('.menu').is(':hidden')) {
+        $('.menu').show('slideToggle');
+    } else {
+        $('.menu').hide('slideToggle');
+    }
+});
+
+
+
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function (e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+});
